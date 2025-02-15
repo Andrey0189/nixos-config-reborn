d5a96351fb166d45f121c0fc02a4807c2c775b04
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2025-02-15 19:45:51 +0300

    Update NixOS modules: modify and add new files

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -3,16 +3,29 @@
     ./audio.nix
     ./bluetooth.nix
     ./boot.nix
+    ./de
     ./env.nix
+    ./firefox.nix
+    ./flatpak.nix
+    ./graphics
     ./home-manager.nix
-    ./hyprland.nix
     ./kernel.nix
+    ./locale.nix
     ./mime.nix
     ./net.nix
     ./nh.nix
     ./nix.nix
+    ./printing.nix
+    # ./spice.nix
+    ./steam.nix
     ./timezone.nix
+    # ./tlp.nix
+    ./trim.nix
     ./user.nix
+    ./virt-manager.nix
+    # ./yggdrasil.nix
+    ./zapret.nix
+    ./zerotierone.nix
     ./zram.nix
   ];
 }
